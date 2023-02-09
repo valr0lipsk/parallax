b467939873eca5666c5dfaf4c16d6b3eba60547e
valeriya.lipskaya <valeriya.lipskaya@softclub.by>
2023-02-09 11:54:56 +0300

    Add custom cursor

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ ScrollSmoother.create({
   wrapper: ".wrapper",
   content: ".content",
 });
+
+
+const cursor = document.querySelector('.cursor');
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+}
+window.addEventListener('mousemove', moveCursor)
